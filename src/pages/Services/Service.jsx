8c09baa1dbff61730a5c6ad8b208357f4423e5fc
sulimankhanswat660-coder <sulimankhanswat660@gmail.com>
2026-05-07 14:24:57 +0500

    implemented ServiceDetails

--- src/pages/Services/Service.jsx
+++ src/pages/Services/Service.jsx
@@ -4,12 +4,14 @@ import Footer from '../../Component/Footer/Footer'
 import Contact from '../../Component/Reuseable/Contact'
 import ServiceHero from './Component/ServiceHero'
 import ServicesList from './Component/ServicesList'
+import ServiceDetails from './Component/ServiceDetails'
 function Service() {
   return (
     <div>
         <Navbar/>
         <ServiceHero/>
         <ServicesList/>
+        <ServiceDetails/>
         <Contact/>
         <Footer/>
     </div>
