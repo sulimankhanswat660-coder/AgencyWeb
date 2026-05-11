e30bccc09ce0a13cd513118154f17d69be6ddfac
sulimankhanswat660-coder <sulimankhanswat660@gmail.com>
2026-05-11 15:21:09 +0500

    implemented NewsCard.jsx implemented FeaturedNews.jsx

--- src/pages/News/Component/FeaturedNews.jsx
+++ src/pages/News/Component/FeaturedNews.jsx
@@ -1,13 +1,16 @@
 import { Box } from '@mui/material'
 import React from 'react'
-import NewsCard from './NewsCard'
+import NewsCard from './Categories'
+import Categories from './Categories'
 function FeaturedNews() {
   return (
     <Box sx={{width:'100%',height:'max-content',}}>
         <Box sx={{width:'90%', mx:'auto',display:'flex',justifyContent:'space-between'}}>
-            <Box>j</Box>
+            <Box>
+              <NewsCard/>
+            </Box>
             <Box sx={{width:{xs:'100%',md:'50%'},justifyItems:{xs:'center'}}}>
-                <NewsCard/>
+                <Categories/>
             </Box>
         
         </Box>
